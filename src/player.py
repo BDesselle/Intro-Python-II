d22e6ae41789301fa52e8b3a4bb012badc1ba031
[Brandon] <desselle.brandon2@gmail.com>
2019-12-06 07:46:25 -0600

    Update player.py

--- src/player.py
+++ src/player.py
@@ -3,3 +3,11 @@
 #
 # name
 # current_room
+
+
+class Player():
+    def __init__(self, currentRoom):
+        self.currentRoom = currentRoom
+
+    def __str__(self):
+        return f"The player's current location: {self.currentRoom}"
